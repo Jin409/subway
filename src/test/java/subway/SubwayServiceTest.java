1de062e7635e9fb455143ea60b0886eb7989db3f
Jin409 <sualng123@gmail.com>
2024-12-10 22:05:35 +0900

    test: 올바른 경로와 거리 및 소요시간을 반환하는지 확인해라

---
+++ src/test/java/subway/SubwayServiceTest.java
@@ -0,0 +1,34 @@
+package subway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import subway.domain.Option;
+import subway.dto.RouteDto;
+import subway.service.StationInitializeService;
+import subway.service.SubwayService;
+import subway.utils.LeastDistanceRouteFinder;
+import subway.utils.MinTimeRequiredRouteFinder;
+
+public class SubwayServiceTest {
+    @Test
+    void 옯바른_경로를_반환한다() {
+        // given
+        StationInitializeService.saveAllStations();
+        StationInitializeService.saveInformation();
+
+        SubwayService subwayService = new SubwayService(new LeastDistanceRouteFinder(),
+                new MinTimeRequiredRouteFinder());
+
+        // when
+        RouteDto route = subwayService.getRoute("남부터미널역", "양재시민의숲역", Option.LEAST_DISTANCE);
+
+        // then
+        assertAll(() -> assertThat(route.getRoute()).isEqualTo(List.of("남부터미널역", "양재역", "양재시민의숲역")),
+                () -> assertThat(route.getDistance()).isEqualTo(16),
+                () -> assertThat(route.getTimeRequired()).isEqualTo(8));
+    }
+}
