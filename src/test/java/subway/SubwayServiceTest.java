c04681ba582e1a18816676dd59624a07d5fa090c
Jin409 <sualng123@gmail.com>
2024-12-10 22:07:51 +0900

    test: 경로가 양방향으로 가능한지 확인해라

--- src/test/java/subway/SubwayServiceTest.java
+++ src/test/java/subway/SubwayServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import subway.domain.Option;
 import subway.dto.RouteDto;
@@ -28,6 +27,24 @@ public class SubwayServiceTest {
 
         // then
         assertAll(() -> assertThat(route.getRoute()).isEqualTo(List.of("남부터미널역", "양재역", "양재시민의숲역")),
+                () -> assertThat(route.getDistance()).isEqualTo(16),
+                () -> assertThat(route.getTimeRequired()).isEqualTo(8));
+    }
+
+    @Test
+    void 양방향의_모든_경로가_가능한지_확인한다() {
+        // given
+        StationInitializeService.saveAllStations();
+        StationInitializeService.saveInformation();
+
+        SubwayService subwayService = new SubwayService(new LeastDistanceRouteFinder(),
+                new MinTimeRequiredRouteFinder());
+
+        // when
+        RouteDto route = subwayService.getRoute("양재시민의숲역", "남부터미널역", Option.LEAST_DISTANCE);
+
+        // then
+        assertAll(() -> assertThat(route.getRoute()).isEqualTo(List.of("양재시민의숲역", "양재역", "남부터미널역")),
                 () -> assertThat(route.getDistance()).isEqualTo(16),
                 () -> assertThat(route.getTimeRequired()).isEqualTo(8));
     }
